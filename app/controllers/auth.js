f8ce803cbf865f6247b386b50172db913ba98744
phuoc <phuoc@huuphuoc.me>
2013-11-05 00:02:54 +0700

    Show flash messages at the sign in page

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -2,27 +2,26 @@ var mongoose = require('mongoose'),
     User     = mongoose.model('user');
 
 exports.signin = function(req, res) {
-    if (req.body.user_name) {
+    if ('post' == req.method.toLowerCase()) {
         User.findOne({ username: req.body.user_name }, function(err, user) {
-            if (err) {
-                // TODO: Add flash message
-                res.redirect('/signin');
-                return;
-            }
-            if (!user) {
-                res.redirect('/signin');
-                return;
+            if (err || !user) {
+                req.flash('error', 'Not found administrator account');
+                return res.redirect('/signin');
             }
             if (!user.verifyPassword(req.body.password)) {
-                res.redirect('/signin');
-                return;
+                req.flash('error', 'Wrong password');
+                return res.redirect('/signin');
             }
 
             req.session.user_name = user.username;
-            res.redirect('/admin');
+            return res.redirect('/admin');
         });
     } else {
         res.render('auth/signin', {
+            messages: {
+                warning: req.flash('error'),
+                success: req.flash('success')
+            },
             title: 'Sign in'
         });
     }
