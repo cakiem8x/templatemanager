2bf7498975d61b273f22fcb5d3f94e2b67cc8ac0
nghuuphuoc <phuoc@huuphuoc.me>
2013-11-08 17:16:05 +0700

    Implement template paging

--- public/asset/js/app.js
+++ public/asset/js/app.js
@@ -34,19 +34,38 @@ angular
             }
         };
     })
+    .directive('tplPagination', function() {
+        return {
+            restrict: 'A',
+            link: function(scope, element, attrs) {
+                var el = angular.element(element);
+                el.on('click', function() {
+                    el.parents('.pagination').find('li').removeClass('active');
+                    el.parent().addClass('active');
+                });
+            }
+        };
+    })
     .constant('defaultCriteria', {
+        page: 1,
         year: null,
         tag: null,
         responsive: null,
         high_resolution: null
     })
     .controller('DemoController', function($rootScope, $scope, $http, defaultCriteria) {
-        $scope.criteria = defaultCriteria;
-        console.log($scope.criteria);
+        $scope.criteria  = defaultCriteria;
 
         $scope.activeTab = 'templates';
         $scope.total     = 0;
         $scope.templates = [];
+
+        // Pagination
+        $scope.pagination = {
+            page: 1,
+            numPages: 1,
+            range: []
+        };
 
         $scope.activateTab = function(tab) {
             $scope.activeTab = tab;
@@ -56,7 +75,23 @@ angular
             $http.post('/filter', $scope.criteria).success(function(data) {
                 $scope.total     = data.total;
                 $scope.templates = data.templates;
+
+                $scope.pagination = {
+                    page: data.page,
+                    numPages: data.numPages,
+                    range: []
+                };
+
+                for (var i = data.startRange; i <= data.endRange; i++) {
+                    $scope.pagination.range.push(i);
+                }
             });
+        };
+
+        $scope.goPage = function(page) {
+            $scope.pagination.page = page;
+            $scope.criteria.page   = page;
+            $scope.filter();
         };
 
         // Load the data
