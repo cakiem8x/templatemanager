47126a90e0d4aee163de0f461e13dd5f301617c4
nghuuphuoc <phuoc@huuphuoc.me>
2013-11-18 09:30:21 +0700

    #24: Set the session collection name as 'session'

--- config/express.js
+++ config/express.js
@@ -45,7 +45,7 @@ module.exports = function(app, config) {
             },
             store: new mongoStore({
                 url: config.db,
-                collection : 'sessions'
+                collection : 'session'
             })
         }));
 
