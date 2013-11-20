e4165ef3c1fec99c1d53a8800b153dca521b72e8
nghuuphuoc <phuoc@huuphuoc.me>
2013-11-20 10:21:24 +0700

    #9: Update Download schema

--- app/models/download.js
+++ app/models/download.js
@@ -2,7 +2,7 @@ var mongoose       = require('mongoose'),
     Schema         = mongoose.Schema,
     downloadSchema = new Schema({
         template: { type : Schema.ObjectId, ref: 'template' },
-        file: { type : Schema.ObjectId, ref: 'template.files' },
+        file: { type : Schema.ObjectId, ref: 'file' },
         user_name: { type: String, default: '' },
         downloaded_date: { type: Date, default: Date.now },
         ip: { type: String, default: '' },
