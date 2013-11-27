4da3d75dcf5c2a1e93f9ab41dce709ccecf8b3b1
nghuuphuoc <phuoc@huuphuoc.me>
2013-11-27 17:13:27 +0700

    Add excluded params to url view helper

--- app/helpers/url.js
+++ app/helpers/url.js
@@ -8,10 +8,15 @@
 var url = require('url'),
     qs = require('querystring');
 
-module.exports = function(req, params) {
+module.exports = function(req, params, excludedParams) {
     var urlParams = qs.parse(url.parse(req.url).query);
     for (var k in params) {
         urlParams[k] = params[k];
+    }
+    if (excludedParams) {
+        for (k in excludedParams) {
+            delete urlParams[excludedParams[k]];
+        }
     }
     return '?' + qs.stringify(urlParams);
 };
